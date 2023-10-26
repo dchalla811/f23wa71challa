c5d5d5306ed2b78d6f95ed27dc2c7100361ac4e3
dchalla811 <challadhruvitha08@gmail.com>
2023-10-25 21:57:50 -0500

    added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -21,9 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+
   next(createError(404));
 });
 
@@ -37,5 +40,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
